b173fccb4b1d140dcfa94c2c800a7de1b699d862
9werayut <werayut.ku@gmail.com>
2019-08-10 18:19:35 +0700

    OpenCV Python Tutorial For Beginners 6 - Setting Camera Parameters in OpenCV Python

--- getting_stared_with_videos.py
+++ getting_stared_with_videos.py
@@ -1,17 +1,20 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-forcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', forcc, 20.0, (640, 480))
+
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+cap.set(3, 3000)
+cap.set(4, 3000)
+
+print(cap.get(3))
+print(cap.get(4))
 
 print(cap.isOpened())
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        out.write(frame)
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow('frame', gray)
@@ -22,5 +25,4 @@ while (cap.isOpened()):
         break
 
 cap.release()
-out.release()
 cv2.destroyAllWindows()
