c9983f16e7a55b23313134db259c759de7c32c23
9werayut <werayut.ku@gmail.com>
2019-08-14 07:05:06 +0700

    OpenCV Python Tutorial For Beginners 9 - More Mouse Event Examples in OpenCV Python

--- mouse_event_opencv_python.py
+++ mouse_event_opencv_python.py
@@ -7,25 +7,19 @@ import cv2
 
 def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, ', ', y)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        strXY = str(x) + ', ' + str(y)
-        cv2.putText(img, strXY, (x, y), font, .5, (255, 255, 0), 2)
-        cv2.imshow('image', img)
-    if event == cv2.EVENT_RBUTTONDOWN:
-        blue = img[y, x, 0]
-        green = img[y, x, 1]
-        red = img[y, x, 2]
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        strBGR = str(blue) + ', ' + str(green) + ', ' + str(red)
-        cv2.putText(img, strBGR, (x, y), font, .5, (0, 255, 255), 2)
-        cv2.imshow('image', img)
+        blue = img[x, y, 0]
+        green = img[x, y, 1]
+        red = img[x, y, 2]
+        cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+        mycolorImage = numpy.zeros((512, 512, 3), numpy.uint8)
+        mycolorImage[:] = [blue, green, red]
+        cv2.imshow('color', mycolorImage)
 
 
 #img = numpy.zeros((512, 512, 3), numpy.uint8)
 img = cv2.imread('lena.jpg')
 cv2.imshow('image', img)
-
+points = []
 cv2.setMouseCallback('image', click_event)
 
 cv2.waitKey(0)
